06170775eaa3fa8ad523fe9f555210e8ce543b42
Sam Sweigart <samuel.sweigart@gmail.com>
2021-06-25 18:41:23 -0400

    status code error

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,13 +2,21 @@ const router = require('express').Router();
 const User  = require('../../models/User');
 
 router.post('/register', async (req, res) => {
+    console.log('req body:',req.body);
+
     try {
-       console.log(req.body)
-    } catch {
-        res.status(404).render('/404');
-    }
-  
-    
+        const newUser = await User.create(req.body);
+        res.status(200).json(newUser);
+        //console.log('req body:',req.body);
+
+        /* req.session.save(() => {
+            req.session.user_id = newUser.id;
+            req.session.logged_in = true;
+            res.status(200).json(newUser);
+        }); */
+    } catch (err) {
+        res.status(404).json(err);
+    };
 });
 
 module.exports = router;
--- public/js/register.js
+++ public/js/register.js
@@ -4,22 +4,23 @@ const registerHandler = async (event) => {
     event.preventDefault();
 
     const fName = $('#f_name').val().trim();
-    const lName = $('#l_name').val().trim();;
-    const email = $('#email').val().trim();;
-    const userName = $('#username').val().trim();;
-    const password = $('#password').val().trim();;
+    const lName = $('#l_name').val().trim();
+    const email = $('#email').val().trim();
+    const userName = $('#username').val().trim();
+    const password = $('#password').val().trim();
     let res;
+    console.log('Input Values:',fName, lName, email, userName, password)
 
-    if (fName && lName && email && userName && password) { 
-        res = await fetch('api/users/register', {
+    if (fName && lName && email && userName && password) {
+        res = await fetch('/register', {
             method: 'POST',
-            body: JSON.stringify({ f_name, l_name, email, username, password }),
+            body: JSON.stringify({ fName, lName, email, userName, password }),
             headers: {'Content-Type': 'application/json' },
         })
      }
      console.log('res:', res);
-         if (res) {
-            document.location.replace('/blogs');
+         if (res.ok) {
+            document.location.replace('/register');
         } else {
             alert('Please fill out all required feilds');
         }
